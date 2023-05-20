9b063f400c0e99042e5a2281ea96598cb297f65e
unknown <ainulrokhman7@gmail.com>
2023-05-20 19:32:40 +0700

    database

--- warung.sql
+++ warung.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 09, 2023 at 10:51 AM
+-- Generation Time: May 20, 2023 at 02:31 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `warung`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `nama_menu` varchar(255) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id`, `id_transaksi`, `nama_menu`, `qty`, `harga`) VALUES
+(1, 1, 'Ale-ale', 1, 5000),
+(2, 1, 'Cocca Cola', 1, 5000),
+(3, 1, 'Jagung Bakar', 1, 5000),
+(4, 2, 'Ale-ale', 1, 5000),
+(5, 3, 'Ale-ale', 5, 5000),
+(6, 3, 'Cocca Cola', 2, 5000),
+(7, 4, 'Ale-ale', 1, 5000);
 
 -- --------------------------------------------------------
 
@@ -44,6 +71,26 @@ INSERT INTO `kategori` (`id`, `nama`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `alamat` longtext NOT NULL,
+  `hp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `member`
+--
+
+INSERT INTO `member` (`id`, `nama`, `alamat`, `hp`) VALUES
+(1, 'Buku Tulis', '', '085227099929');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `menu`
 --
 
@@ -61,20 +108,57 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`id`, `kategori_id`, `nama`, `stok`, `harga`, `gambar`) VALUES
-(1, 14, 'Kripik Kentang', 10, 5000, 'default.jpg'),
+(1, 16, 'Kripik Kentang', 10, 5000, 'default.jpg'),
 (2, 14, 'Jagung Bakar', 10, 5000, 'default.jpg'),
 (4, 15, 'Ale-ale', 10, 5000, 'default.jpg'),
 (5, 15, 'Cocca Cola', 10, 5000, 'default.jpg'),
 (8, 16, 'Sabun Mandi', 200, 10000, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `invoice` varchar(20) NOT NULL,
+  `total` int(11) NOT NULL,
+  `bayar` int(11) NOT NULL,
+  `kembali` int(11) NOT NULL,
+  `id_member` int(11) DEFAULT NULL,
+  `waktu` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `invoice`, `total`, `bayar`, `kembali`, `id_member`, `waktu`) VALUES
+(1, '2023/INV/00000', 15000, 15000, 0, NULL, '2023-05-16 12:23:33'),
+(3, '2023/INV/00002', 35000, 35000, 0, NULL, '2023-05-17 21:57:44'),
+(4, '2023/INV/00003', 5000, 500, -4500, 1, '2023-05-17 22:22:33');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -84,8 +168,20 @@ ALTER TABLE `menu`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -94,10 +190,22 @@ ALTER TABLE `kategori`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
